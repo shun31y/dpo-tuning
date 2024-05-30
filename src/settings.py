4e9afa08cb80af00f9b20f1443d429a6adc6ce32
shun31y <shun.kato@elyza.ai>
2024-05-30 22:16:16 +0900

    dotenvから環境変数読み込むsettings.pyを追加

---
+++ src/settings.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+dotenv_path = Path(__file__).parent.parent / ".env"
+load_dotenv(verbose=True)
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
